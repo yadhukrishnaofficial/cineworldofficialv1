2f86302003d92ca14de86326d24a5de075f2fce7
Yadhu Krishna <85838175+yadhukrishnaofficial@users.noreply.github.com>
2021-09-06 20:49:03 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"⚜️Support @cine_world_official⚜️ Found {(len_results)} Results For Your Result: <code>{query}</code>",
+                text=f" Found {(len_results)} Results For Your Result: <code>{query}</code> ⚜️Support @cine_world_official⚜️",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
